b0d33e1321d919d0d1ec7420ece9560e4d40bcc7
FlightDude737 <drewcrocker23@gmail.com>
2020-05-15 22:01:15 +0000

    added dp of ace it

--- index.js
+++ index.js
@@ -6,6 +6,8 @@ const http = require('http').Server(app)
 const io = require('socket.io')(http)
 const mysql = require('mysql')
 
+var serverReload = false
+
 var con = mysql.createConnection({
   host: process.env.HOST,
   database: process.env.DATABASE,
@@ -18,6 +20,16 @@ var emptyData = false
 con.connect((err) => {
   if (err) throw err;
   console.log('Connected to database.')
+  con.query("CREATE TABLE post (name VARCHAR(255), title VARCHAR(255), message VARCHAR(255), rating VARCHAR(255), id VARCHAR(255))", (err, result) => {
+    if (err) {
+      if (err.sqlMessage == "Table 'post' already exists") {
+        console.log("Overriding table creation key...")
+        setTimeout(() => {
+          console.log("\nOverride complete")
+        }, 1000)
+      }
+    }
+  })
   con.query('SELECT * FROM post', (err, result, fields) => {
     if (err) throw err
     if (result == "[]"){
@@ -28,15 +40,18 @@ con.connect((err) => {
 })
 
 io.on('connection', (socket) => {
+  var ratingInfo = JSON.parse(fs.readFileSync('public/rating.json'))
+  if (ratingInfo[0].comments == "0"){
+    socket.emit('return-rating', 'nocomments')
+  } else {
+    var offRating = Math.round(Number(ratingInfo[0].rate) / Number(ratingInfo[0].comments)).toString()
+    var offComments = ratingInfo[0].comments
+    socket.emit('return-rating', offRating)
+  }
   if (emptyData = true) {
     con.query('SELECT * FROM post', (err, result, fields) => {
       if (err) throw err
       result.forEach((post) => {
-        console.log(result[0].name)
-        console.log(result[0].title)
-        console.log(result[0].message)
-        console.log(result[0].rating)
-        console.log(result[0].id)
         socket.emit('post-in-socket', post.name, post.title, post.message, post.rating, post.id)
       })
     })
@@ -45,6 +60,16 @@ io.on('connection', (socket) => {
     io.emit('added-comment', num)
   })
   socket.on('post', (name, title, message, rating, id) => {
+    var data = JSON.parse(fs.readFileSync('public/rating.json'))
+    var emptydata = []
+    var comments = (Number(data[0].comments) + 1).toString()
+    var vanrate = Number(data[0].rate) + Number(rating)
+    var rate = vanrate
+    emptydata.push({
+      comments,
+      rate
+    })
+    fs.writeFileSync('public/rating.json', JSON.stringify(emptydata))
     var sqlInformation = "INSERT INTO post (name, title, message, rating, id) VALUES ('" + name + "', '" + title + "', '" + message + "', '" + rating + "', '" + id + "')"
     con.query(sqlInformation, (err, result) => {
       if (err) {
@@ -66,6 +91,38 @@ app.get('/', (req, res) => {
   })
 })
 
+app.get('/remove', (req, res) => {
+  var data = JSON.parse(fs.readFileSync('public/rating.json'))
+  var value = (Number(data[0].comment) - 1).toString()
+  var empty = []
+  var delStarVal = 0
+  var rating = data[0].rate
+  con.query("SELECT * FROM post WHERE id = '" + req.query.post + "'", (err, result, fields) => {
+    delStarVal = result[0].rating
+    con.query("DELETE FROM post where id = '" + req.query.post + "'", (err, buffer) => {
+      if (err) {
+        console.log(err)
+      }
+      else {
+        console.log(result)
+        var currRate = data[0].rate
+        var newRate = Number(currRate) - result[0].rating
+        empty.push({
+          comments: (Number(data[0].comments) - 1).toString(),
+          rate: (Number(currRate) - result[0].rating).toString()
+        })
+        var newEmpty = JSON.stringify(empty)
+        fs.writeFileSync('public/rating.json', newEmpty)
+        console.log("Status: query " + req.query.post + " removed")
+        res.send("<script>window.location = '/'</script>")
+      }
+    })
+  })
+})
+
 http.listen(3000, () => {
   console.log('listening...')
+  setTimeout(() => {
+    io.emit('server-update')
+  }, 2000)
 })
